48056acffa7c76c2a1bdb200de4a0026a293e625
Amina Fayyaz <aminafayyaz595@gmail.com>
2026-03-16 10:01:11 +0500

    added two files

---
+++ Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    
+    public static void main(String[] args){
+
+        Students s1 = new Students();
+        s1.name = "Amina Fayyaz";
+        s1.rollNumber = 5033;
+
+        Students s2 = new Students();
+        s2.name = "Fatima";
+        s2.rollNumber = 5061;
+
+        s1.displayInfo();
+        s2.displayInfo();
+
+    }
+}
---
+++ Students.java
@@ -0,0 +1,15 @@
+public class Students {
+
+    String name;
+    int rollNumber;
+
+    public void displayInfo(){
+
+        System.out.println("Student Name is :" + name);
+        System.out.println("RollNumber is :" + rollNumber);
+
+        System.out.println("-------------------------------------------------------");
+
+    }
+    
+}
