b2b77958770488cb7baf922f6315cf1a62a4e409
Amina Fayyaz <aminafayyaz595@gmail.com>
2026-04-06 11:17:01 +0500

    added two file of lab 6

--- Main.java
+++ Main.java
@@ -1,17 +1,22 @@
 public class Main {
-    
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
-        Students s1 = new Students();
-        s1.name = "Amina Fayyaz";
-        s1.rollNumber = 5033;
+        BankAccount account1 = new BankAccount("Amina", 0.0);
+        BankAccount account2 = new BankAccount("Fatima", 1000.0);
 
-        Students s2 = new Students();
-        s2.name = "Fatima";
-        s2.rollNumber = 5061;
+        account2.deposit(2000);
+        account2.withdraw(200);
+        account2.displayAccountInfo();
+       
+        account1.withdraw(-100);
+        account1.withdraw(50);
+        account1.displayAccountInfo();
 
-        s1.displayInfo();
-        s2.displayInfo();
-
+        System.out.println("Account Holder: " + account1.getAccountHolder());
+        System.out.println("Final Details: " + account1.getBalance());
+        
+        System.out.println("Account Holder: " + account2.getAccountHolder());
+        System.out.println("Final Details: " + account2.getBalance());
     }
+
 }
